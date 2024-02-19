f8fcaa25fec4003fc83f8ccff9e216cce8ad1369
Vipul Puttaswamy Gowda <vipulp@pdx.edu>
2024-02-18 16:15:08 -0800

    FEAT: updated async with api integration

--- InMemory/cache.py
+++ InMemory/cache.py
@@ -1,19 +1,22 @@
 import time
 import asyncio
-import hashlib    
+import hashlib
+import httpx   
 
 class DistributedCache:
   def __init__(self, num_nodes = 5):
     self.nodes = [Cache() for _ in range(num_nodes)]
   
   def get_node_key(self, key):
+    print(key)
     hash_value = int(hashlib.md5(key.encode()).hexdigest(), 16)
     return self.nodes[hash_value % len(self.nodes)]
   
   async def get(self, key):
     await self.getAll()
     node = self.get_node_key(key)
-    await node.get(key)
+    res = await node.get(key)
+    print(res)
 
   async def put(self, key, value, ttl = 20):
     node = self.get_node_key(key)
@@ -29,7 +32,6 @@ class DistributedCache:
       await node.cleanup()
   
 class LRUCache:
-  
   def __init__(self):
     self.csize = 2
     self.dq = []
@@ -61,6 +63,7 @@ class Cache:
       if self.cache[key]["expiry"] < time.time():
         self.delete(key)
       print("Getting the key", key)
+      print(self.cache[key])
   
   async def put(self, key, value, ttl= 0):
     time.sleep(1)
@@ -72,8 +75,11 @@ class Cache:
       self.delete(last)
       self.lru_cache.pop()
     if key not in self.cache:
-      self.cache[key] = { "value": value, "expiry": expiration_time }
-      print("Inserted the key", key)
+      async with httpx.AsyncClient(timeout=60.0) as client:
+        value = await run_url(client, f'https://api.punkapi.com/v2/beers?page={value + 1}')
+        print("API resp ==>",value)
+        self.cache[key] = { "value": value, "expiry": expiration_time }
+        print("Inserted the key", key)
   
   async def delete(self,key):
     if key in self.cache:
@@ -88,27 +94,37 @@ class Cache:
       print("Cleaning up key: ",key)
       await self.delete(key)
 
+
+async def run_url(client, url):
+  resp = await client.get(url,follow_redirects=True)
+  print(resp.text)
+  if resp.text:
+    print(resp.text)
+    return resp.text
+  
+
 async def simulate_concurrent_requests(cache, num_requests):
   tasks = []
-  file = open("../size.txt").read()
+  # file = open("../size.txt").read()
   for i in range(num_requests):
     key = f"key{i}"
-    value = file
-    tasks.append(cache.put(key, value, 30))
-    if i % 5 == 0:
-      tasks.append(cache.get(key))
-    if i % 4 == 0:
-      tasks.append(cache.cleanup())
+    tasks.append(cache.put(key, i, 30))
+    # if i % 5 == 0:
+    #   tasks.append(cache.get(key))
+    # if i % 4 == 0:
+    #   tasks.append(cache.cleanup())
   s = time.perf_counter()
   result = await asyncio.gather(*tasks)
   elapsed = time.perf_counter() - s
   print(elapsed)
+  res = await cache.get('key2')
+  print(res)
   return result
   
 
 async def main():
   cache = DistributedCache(5)
-  return await simulate_concurrent_requests(cache,2000)
+  return await simulate_concurrent_requests(cache,5)
 
 def runAsync(): 
   return(asyncio.run(main()))
